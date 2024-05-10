80579852e98d9f1825c5f9d72017cc659a861612
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2024-05-10 17:13:15 +0100

    engine refactoring

--- engine/core/Engine.js
+++ engine/core/Engine.js
@@ -1,206 +1,94 @@
+import Emitter from '../abstract/Emitter.js';
+import { debounce, requestFullscreenSafely, requestPointerLockSafely } from '../utils.js';
+import Ticker from './Ticker.js';
+import Game from './game/Game.js';
+import InputManager from './input/InputManager.js';
+import Renderer from './renderer/Renderer.js';
+
 /**
  * @typedef {Object} EngineSpec
  * @property {HTMLCanvasElement} canvas
- * @property {import('./game/Game.js').GameSpec} game
  */
-
-import Game from './game/Game.js';
-import InputManager from './input/InputManager.js';
-import Ticker from './Ticker.js';
-import Renderer from './renderer/Renderer.js';
-import { debounce } from '../utils.js';
 
 /**
  * @module
  *
  * The Engine class is the central context, holding the game state and logic.
  */
-export default class Engine {
+export default class Engine extends Emitter {
   /** @type {Game} */
-  #game;
+  game;
 
   /** @type {InputManager} */
-  #input;
+  input;
 
   /** @type {Renderer} */
-  #renderer;
+  renderer;
 
   /** @type {Ticker} */
-  #ticker;
+  ticker;
 
   /**
    * Create a new Engine.
    * @param {EngineSpec} spec
    */
   constructor(spec) {
-    const { canvas, game } = spec;
-    this.#ticker = new Ticker();
-    this.#input = new InputManager();
-    this.#game = new Game(game);
-    this.#renderer = new Renderer(canvas, game.tileSize);
+    super();
+    this.ticker = new Ticker();
+    this.input = new InputManager();
+    this.game = new Game();
+    this.renderer = new Renderer(spec.canvas);
 
-    this.#ticker.on('preUpdate', this.#preUpdate, { context: this });
-    this.#ticker.on('postUpdate', this.#postUpdate, { context: this });
-    this.#ticker.on('update', this.#update, { context: this });
-  }
+    // note: these go here to ensure they run first
+    this.ticker.on('preUpdate', this.#preUpdate, { context: this });
+    this.ticker.on('postUpdate', this.#postUpdate, { context: this });
+    this.ticker.on('update', this.#update, { context: this });
 
-  #addEventListeners() {
-    [
-      {
-        event: 'resize',
-        el: globalThis,
-        handler: debounce(() => this.#renderer.canvas.resizeDOM(), 16),
-      },
-      { event: 'blur', el: globalThis, handler: () => this.#input.reset() },
-      { event: 'focus', el: globalThis, handler: () => {} },
-      {
-        event: 'mousedown',
-        el: this.#renderer.canvas.element,
-        handler: async (/** @type {MouseEvent} */ e) => {
-          e.preventDefault();
-          if (e.button === 0) {
-            this.#input.mouse.leftBtn = true;
-            if (!document.pointerLockElement) {
-              // @ts-ignore
-              await this.#renderer.canvas.element.requestPointerLock({
-                unadjustedMovement: true,
-              });
-            }
-          } else if (e.button === 2) {
-            this.#input.mouse.rightBtn = true;
-          }
-        },
-      },
-      {
-        event: 'mouseup',
-        el: this.#renderer.canvas.element,
-        handler: (/** @type {MouseEvent} */ e) => {
-          e.preventDefault();
-          if (e.button === 0) {
-            this.#input.mouse.leftBtn = false;
-          } else if (e.button === 2) {
-            this.#input.mouse.rightBtn = false;
-          }
-        },
-      },
-      {
-        event: 'mousemove',
-        el: this.#renderer.canvas.element,
-        handler: (/** @type {MouseEvent} */ e) => {
-          const { mouse } = this.#input;
-          e.preventDefault();
-          if (globalThis.document.pointerLockElement === e.target) {
-            mouse.moveX += e.movementX;
-            mouse.moveY += e.movementY;
-          } else if (globalThis.document.fullscreenElement === e.target) {
-            mouse.x = e.pageX / window.innerWidth;
-            mouse.y = e.pageY / window.innerHeight;
-          } else {
-            // @ts-ignore
-            const offset = e.target.getBoundingClientRect();
-            mouse.x = (e.pageX - offset.left) / document.body.clientWidth;
-            mouse.y = (e.pageY - offset.top) / document.body.clientHeight;
-          }
-        },
-      },
-      {
-        event: 'contextmenu',
-        el: this.#renderer.canvas.element,
-        handler: (/** @type {MouseEvent} */ e) => e.preventDefault(),
-      },
-    ].forEach(({ event, el, handler }) => {
-      // @ts-ignore
-      el.addEventListener(event, handler);
-    });
+    Object.freeze(this);
   }
 
   /**
    * Setup the engine elements and load the game assets.
-   * @returns {Promise<Engine>}
+   * @param {import('./game/Game.js').GameSpec} game
+   * @returns {Promise<this>}
    */
-  async init() {
-    this.#addEventListeners();
+  async init(game) {
+    // add external event listeners
+    globalThis.onresize = debounce(() => this.renderer.canvas.resizeDOM(), this.ticker.tickDuration);
+    globalThis.onblur = () => this.input.reset();
+    globalThis.onfocus = () => {};
+    globalThis.document.onfullscreenchange = () => this.input.mouse.reset();
+    globalThis.document.onpointerlockchange = () => {
+      this.input.mouse.reset();
+      if (globalThis.document?.pointerLockElement === this.renderer.canvas.element) {
+        // reset the mouse position to the center of the canvas
+        this.input.mouse.x = this.renderer.canvas.element.width / 2;
+        this.input.mouse.y = this.renderer.canvas.element.height / 2;
+      }
+    };
 
-    this.#game.on('mapChange', () => {
-      this.#renderer.camera.reset();
-      this.#renderer.camera.centerOn(
-        Math.round(this.#game.player.position.x),
-        Math.round(this.#game.player.position.y)
-      );
+    globalThis.onkeydown = (e) => this.input.keyboard.keyDown(e);
+    globalThis.onkeyup = (e) => this.input.keyboard.keyUp(e);
+
+    const { element } = this.renderer.canvas;
+    element.onmousedown = (e) => mouseDown(this, e);
+    element.onmouseup = (e) => mouseUp(this, e);
+    element.onmousemove = (e) => mouseMove(this, e);
+    element.oncontextmenu = (e) => e.preventDefault();
+
+    // add internal event listeners
+    this.game.on('mapChange', () => {
+      this.renderer.camera.reset();
+      this.renderer.camera.centerOn(Math.round(this.game.player.position.x), Math.round(this.game.player.position.y));
     });
 
-    this.#ticker.on('preUpdate', () => {
-      if (this.#input.keyboard.isDown('KeyW') || this.#input.keyboard.isDown('ArrowUp')) {
-        this.#game.player.walkDirection = 1;
-      } else if (this.#input.keyboard.isDown('KeyS') || this.#input.keyboard.isDown('ArrowDown')) {
-        this.#game.player.walkDirection = -1;
-      } else {
-        this.#game.player.walkDirection = 0;
-      }
+    // initialize the engine elements
+    this.input.init();
+    this.renderer.init(game.tileSize);
 
-      if (this.#input.keyboard.isDown('KeyA') || this.#input.keyboard.isDown('ArrowLeft')) {
-        this.#game.player.turnDirection = -1;
-      } else if (this.#input.keyboard.isDown('KeyD') || this.#input.keyboard.isDown('ArrowRight')) {
-        this.#game.player.turnDirection = 1;
-      } else {
-        this.#game.player.turnDirection = 0;
-      }
+    // load the game assets
+    await this.game.init(game);
 
-      if (this.#input.keyboard.isDown('Equal')) {
-        this.#renderer.camera.zoomBy(1.05);
-      } else if (this.#input.keyboard.isDown('Minus')) {
-        this.#renderer.camera.zoomBy(0.95);
-      }
-    });
-
-    /*  this.#ticker.on('postUpdate', () => {
-      console.log(this.#ticker.getFPS());
-    }); */
-
-    this.#input.init();
-    this.#renderer.init();
-    await this.#game.init();
-
-    return this;
-  }
-
-  /**
-   * @returns {this}
-   */
-  start() {
-    this.#ticker.start();
-    return this;
-  }
-
-  /**
-   * @returns {this}
-   */
-  stop() {
-    this.#ticker.stop();
-    return this;
-  }
-
-  /**
-   * @returns {this}
-   */
-  pause() {
-    this.#ticker.pause();
-    return this;
-  }
-
-  /**
-   * @returns {this}
-   */
-  resume() {
-    this.#ticker.resume();
-    return this;
-  }
-
-  /**
-   * @param {number} index
-   */
-  changeMap(index) {
-    this.#game.changeMap(index);
     return this;
   }
 
@@ -208,7 +96,37 @@ export default class Engine {
    * @returns {void}
    */
   #preUpdate() {
-    this.#input.update();
+    if (this.input.keyboard.isDown('KeyW')) {
+      this.game.player.walkDirection = 1;
+    } else if (this.input.keyboard.isDown('KeyS')) {
+      this.game.player.walkDirection = -1;
+    } else {
+      this.game.player.walkDirection = 0;
+    }
+
+    if (this.input.keyboard.isDown('KeyA')) {
+      this.game.player.turnDirection = -1;
+    } else if (this.input.keyboard.isDown('KeyD')) {
+      this.game.player.turnDirection = 1;
+    } else {
+      this.game.player.turnDirection = 0;
+    }
+
+    if (this.input.keyboard.isDown('Equal')) {
+      this.renderer.camera.zoomBy(1.05);
+    } else if (this.input.keyboard.isDown('Minus')) {
+      this.renderer.camera.zoomBy(0.95);
+    }
+
+    if (this.input.keyboard.isDown('ArrowUp')) {
+      this.renderer.camera.y -= 5;
+    } else if (this.input.keyboard.isDown('ArrowDown')) {
+      this.renderer.camera.y += 5;
+    } else if (this.input.keyboard.isDown('ArrowLeft')) {
+      this.renderer.camera.x -= 5;
+    } else if (this.input.keyboard.isDown('ArrowRight')) {
+      this.renderer.camera.x += 5;
+    }
   }
 
   /**
@@ -216,7 +134,9 @@ export default class Engine {
    * @returns {void}
    */
   #postUpdate(alpha) {
-    this.#renderer.draw2d(this.#game);
+    this.renderer.draw2d(this.game);
+    this.renderer.render();
+    this.input.update();
   }
 
   /**
@@ -224,6 +144,45 @@ export default class Engine {
    * @returns {void}
    */
   #update(delta) {
-    this.#game.update(delta);
+    this.game.update(delta);
+  }
+}
+
+async function mouseDown(/** @type {Engine} */ engine, /** @type {MouseEvent} */ e) {
+  e.preventDefault();
+  const { mouse } = engine.input;
+  const element = engine.renderer.canvas.element;
+  if (e.button === 0) {
+    mouse.leftBtn = true;
+    await requestPointerLockSafely(element);
+    // await requestFullscreenSafely(element);
+  } else if (e.button === 2) {
+    mouse.rightBtn = true;
+  }
+}
+
+function mouseUp(/** @type {Engine} */ engine, /** @type {MouseEvent} */ e) {
+  const { mouse } = engine.input;
+  e.preventDefault();
+  if (e.button === 0) {
+    mouse.leftBtn = false;
+  } else if (e.button === 2) {
+    mouse.rightBtn = false;
+  }
+}
+
+function mouseMove(/** @type {Engine} */ engine, /** @type {MouseEvent} */ e) {
+  const { mouse } = engine.input;
+  e.preventDefault();
+  if (globalThis.document.pointerLockElement === e.target) {
+    mouse.dx += e.movementX;
+    mouse.dy += e.movementY;
+  } else if (globalThis.document.fullscreenElement === e.target) {
+    mouse.x = e.pageX / window.innerWidth;
+    mouse.y = e.pageY / window.innerHeight;
+  } else {
+    const offset = engine.renderer.canvas.boundingClientRect;
+    mouse.x = (e.clientX - offset.left) / offset.width;
+    mouse.y = (e.clientY - offset.top) / offset.height;
   }
 }
